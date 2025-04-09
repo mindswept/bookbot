3d40c3ae4fbb198e7fdd336b0f53ea4c073e174f
mindswept <79777938+mindswept@users.noreply.github.com>
2025-04-08 22:16:22 -0400

    add ability to take text from a book and return the number of times each character appears

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_chars_appear
 
 
 def get_book_text(filepath):
@@ -12,8 +12,10 @@ def get_book_text(filepath):
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     num_words = get_num_words(book_text)
+    num_chars = get_chars_appear(book_text)
     print(f"{num_words} words found in the document")
-    print(book_text)
+    print(num_chars)
+    # print(book_text)
 
 
 main()
--- stats.py
+++ stats.py
@@ -3,3 +3,16 @@ def get_num_words(book_text):
     num_words = len(words)
 
     return num_words
+
+
+def get_chars_appear(book_text):
+    lower_case_chars = book_text.lower()
+    num_chars = {}
+
+    for char in lower_case_chars:
+        if char in num_chars:
+            num_chars[char] += 1
+        else:
+            num_chars[char] = 1
+
+    return num_chars
